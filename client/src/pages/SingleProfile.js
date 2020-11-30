9ea45e9932c318d13022a2c9c7d824d27fd707d8
Crankyshaft Justdoit <mani4pmn@gmail.com>
2020-11-30 15:06:15 +0530

    Inital commit

---
+++ client/src/pages/SingleProfile.js
@@ -0,0 +1,138 @@
+import React, { useContext, useState, useRef } from 'react';
+import gql from 'graphql-tag';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import moment from 'moment';
+import {
+  Button,
+  Card,
+  Form,
+  Grid,
+  Image,
+  Icon,
+  Label,
+  Transition
+} from 'semantic-ui-react';
+
+import { AuthContext } from '../context/auth';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
+import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
+import LikeButton from '../components/LikeButton';
+import DeleteButton from '../components/DeleteButton';
+import MyPopup from '../util/MyPopup';
+
+function SingleProfile(props) {
+  const postId = props.match.params.postId;
+  const userId = props.match.params.username;
+  const createdAt = props.match.params.createdAt;
+  const { user } = useContext(AuthContext);
+  const commentInputRef = useRef(null);
+  
+
+  const [comment, setComment] = useState('');
+
+  const {
+    loading,
+    data: { getPosts: posts } = {}
+  } = useQuery(FETCH_POSTS_QUERY);
+
+
+  const [submitComment] = useMutation(SUBMIT_COMMENT_MUTATION, {
+    update() {
+      setComment('');
+      commentInputRef.current.blur();
+    },
+    variables: {
+      postId,
+      body: comment
+    }
+  });
+
+
+  const pageOwnerPosts = posts.filter(function (f) {
+    return f.username === userId;
+  });
+
+
+  function deletePostCallback() {
+    props.history.push('/');
+  }
+
+  let postMarkup = (
+      <Grid columns={1}>
+      <Grid.Column className="posts">
+        {loading ? (
+          <h1>Loading posts</h1>
+        ) : (
+          <div className="posts-container">
+          <Transition.Group>
+            <Card
+              className="profile-header"
+              image="https://react.semantic-ui.com/images/avatar/large/molly.png"
+              header={userId}
+              meta={createdAt}
+              description=''
+              extra=''
+            />
+            {pageOwnerPosts && 
+              pageOwnerPosts.map((post) => (
+                <Grid>
+                <Grid.Column key={post.id} style={{ marginBottom: 20, marginTop: 20  }}>
+                  <PostCard post={post} />
+                </Grid.Column>
+                </Grid>
+              ))}
+          </Transition.Group>
+          </div>
+        )}
+      </Grid.Column>
+
+      </Grid>
+    );
+    return (
+      postMarkup
+    )
+    
+    
+  };
+  
+
+
+const SUBMIT_COMMENT_MUTATION = gql`
+  mutation($postId: String!, $body: String!) {
+    createComment(postId: $postId, body: $body) {
+      id
+      comments {
+        id
+        body
+        createdAt
+        username
+      }
+      commentCount
+    }
+  }
+`;
+
+const FETCH_POST_QUERY = gql`
+  query($postId: ID!) {
+    getPost(postId: $postId) {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
+
+export default SingleProfile;
